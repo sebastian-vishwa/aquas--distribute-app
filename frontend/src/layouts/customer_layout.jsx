fd28804d8dda59d07d963a7eb434bbc3c37a58c9
GaganiTharusha <gaganitharusha2004@gmail.com>
2026-09-05 12:23:54 +0530

    remove reg.footer

--- frontend/src/layouts/customer_layout.jsx
+++ frontend/src/layouts/customer_layout.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import NavbarRegCus from '../components/customer/navbar_reg_cus';
-import FooterRegCus from '../components/common/footer_reg_cus';
 import '../pages/reg_customer/portal.css'; // We will create this next!
 
 export default function CustomerLayout() {
@@ -11,7 +10,6 @@ export default function CustomerLayout() {
       <main style={{ flex: 1 }}>
         <Outlet />
       </main>
-      <FooterRegCus />
     </div>
   );
 }
