6f1036f1b1bf65e32c9e723e19c9f707e430ac00
GaganiTharusha <gaganitharusha2004@gmail.com>
2026-09-05 14:51:40 +0530

    fix css parts

--- frontend/src/layouts/customer_layout.jsx
+++ frontend/src/layouts/customer_layout.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import NavbarRegCus from '../components/customer/navbar_reg_cus';
-import '../pages/reg_customer/portal.css'; // We will create this next!
 
 export default function CustomerLayout() {
   return (
