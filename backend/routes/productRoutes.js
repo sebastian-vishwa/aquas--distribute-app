7d0bdcfe166ae732b78630073cf96a05b8a7f13b
GaganiTharusha <gaganitharusha2004@gmail.com>
2026-09-06 21:14:01 +0530

    product routes backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { addProduct, getProducts } = require('../controllers/productController');
+const { addProduct, getProducts , getProductById,updateProduct,deleteProduct} = require('../controllers/productController');
+const auth = require('../middleware/auth');
+const requireManager = require('../middleware/requireManager');
 
-// POST request to add a product
-router.post('/add', addProduct);
-
-// GET request to fetch all products for your inventory table
 router.get('/', getProducts);
+router.get('/:id', getProductById);
+
+router.post('/add', auth, requireManager, addProduct);
+router.put('/:id', auth, requireManager, updateProduct);
+router.delete('/:id', auth, requireManager, deleteProduct);
 
 module.exports = router;
